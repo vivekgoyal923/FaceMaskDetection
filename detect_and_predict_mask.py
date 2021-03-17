6984217bcfca1ba80aa0e0ffefc7b38e47f166fb
Vivek Goyal <vivekgoyal923@gmail.com>
2021-03-17 15:29:27 +0800

    Moved Variables at top

--- detect_and_predict_mask.py
+++ detect_and_predict_mask.py
@@ -8,8 +8,7 @@ def detect_and_predict_mask(frame, faceNet, maskNet, resNet):
     # grab the dimensions of the frame and then construct a blob
     # from it
     (h, w) = frame.shape[:2]
-    blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300),
-        (104.0, 177.0, 123.0))
+    blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300), (104.0, 177.0, 123.0))
     # pass the blob through the network and obtain the face detections
     faceNet.setInput(blob)
     detections = faceNet.forward()
--- detect_mask.py
+++ detect_mask.py
@@ -14,13 +14,19 @@ import os
 import math
 
 
+face_detect_prototxt = "model/deploy.prototxt"
+face_detect_caffemodel = "model/res10_300x300_ssd_iter_140000.caffemodel"
+mask_detect_model = "model/trained_model"
+DetectedFolder = "DetectedFaces/"
+TestImage = "TestImage/"
+
 def detect_mask():
     # load our serialized face detector model from disk
     print("[INFO] loading face detector model...")
-    faceNet = cv2.dnn.readNet("model/deploy.prototxt", "model/res10_300x300_ssd_iter_140000.caffemodel")
+    faceNet = cv2.dnn.readNet(face_detect_prototxt, face_detect_caffemodel)
     # load the face mask detector model from disk
     print("[INFO] loading face mask detector model...")
-    maskNet = load_model("model/trained_model")
+    maskNet = load_model(mask_detect_model)
     resNet = VGGFace(model='resnet50',
                     input_shape=(224, 224, 3),
                     pooling='avg')
@@ -47,17 +53,17 @@ def detect_mask():
                 new_time = datetime.now()
                 old_time = ""
                 count = 0
-                listOfTestPics = os.listdir("TestImage/")
+                listOfTestPics = os.listdir(TestImage)
                 flag = 0
                 for testImage in listOfTestPics:
-                    imageToCompare = cv2.imread("TestImage/" + testImage)
+                    imageToCompare = cv2.imread(TestImage + testImage)
                     frame_to_compare = imutils.resize(imageToCompare, width=400)
                     model_score2 = ei.extract_face_from_image(frame_to_compare, faceNet, resNet)
                     print(cosine(score, model_score2))
                     if cosine(score, model_score2) <= 0.000007:
                         flag = 1
                         name = testImage.split(".")[0]
-                        listOfPics = os.listdir("DetectedFaces/")
+                        listOfPics = os.listdir(DetectedFolder)
                         time_dif = math.inf
                         for pic in listOfPics:
                             if name in pic:
@@ -69,22 +75,22 @@ def detect_mask():
                         print(old_time, new_time)
                         if 500 < time_dif < 1000:
                             print("Update on Dashboard")
-                            if os.path.exists(f"DetectedFaces/{name} {old_time} {count}.jpg"):
-                                os.remove(f"DetectedFaces/{name} {old_time} {count}.jpg")
+                            if os.path.exists(f"{DetectedFolder}{name} {old_time} {count}.jpg"):
+                                os.remove(f"{DetectedFolder}{name} {old_time} {count}.jpg")
                             count += 1
-                            cv2.imwrite(f"DetectedFaces/{name} {new_time.strftime('%y%m%d%H%M%S')} {count}.jpg", croppedImage)
+                            cv2.imwrite(f"{DetectedFolder}{name} {new_time.strftime('%y%m%d%H%M%S')} {count}.jpg", croppedImage)
                         elif time_dif > 1000:
                             print("New Image Uploaded in Detected Folder")
-                            if os.path.exists(f"DetectedFaces/{name} {old_time} {count}.jpg"):
-                                os.remove(f"DetectedFaces/{name} {old_time} {count}.jpg")
-                            cv2.imwrite(f"DetectedFaces/{name} {new_time.strftime('%y%m%d%H%M%S')} {count}.jpg", croppedImage)
+                            if os.path.exists(f"{DetectedFolder}{name} {old_time} {count}.jpg"):
+                                os.remove(f"{DetectedFolder}{name} {old_time} {count}.jpg")
+                            cv2.imwrite(f"{DetectedFolder}{name} {new_time.strftime('%y%m%d%H%M%S')} {count}.jpg", croppedImage)
                         else:
                             print("Waiting for 2 min")
                         break
                 if flag == 0:
                     new_name = input("Please Enter the name for new Face:")
                     if new_name != "ignore":
-                        cv2.imwrite(f"TestImage/{new_name}.jpg", frame)
+                        cv2.imwrite(f"{TestImage}{new_name}.jpg", frame)
         cv2.imshow("Frames", frame)
         key = cv2.waitKey(1) & 0xFF
         if key == ord("q"):
